dd8f7cc775386d931de9c0b25cccb1d5bd2df8ed
shobana <shobana.sekar@vanavil-systems.com>
2019-09-03 12:20:58 +0530

    try catch changes

--- admin/admin-routes.js
+++ admin/admin-routes.js
@@ -22,57 +22,65 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage })
 
 router.post('/shiftupload', upload.single('file'), (req, res, next) => {
-    var arr= [];
+    var arr = [];
     const file = req.file;
-    const workbook = xlsx.readFile(file.path, {type: 'binary'});
+    const workbook = xlsx.readFile(file.path, { type: 'binary' });
     ws = workbook.Sheets[workbook.SheetNames[0]];
     const sheet_name = workbook.SheetNames;
     const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name[0]]);
-    
-    for (var i in data) {
-        var newDate =  new Date((data[i].Date - (25567 + 2))*86400*1000);
-          var convertedDate = dateFormat(newDate, "mm/dd/yyyy"); 
-        var userObj = {
-            date : convertedDate,
-            shift1:data[i].Shift1,
-            shift2:data[i].Shift2,
-            shift3:data[i].Shift3,
-            shift4:data[i].Shift4,
-            remark:data[i].Remark
-        }
-            admin.create(userObj, (err, user) =>{
-            if (err) {
-                res.json({
-                    success: false,
-                    message: 'Something went Wrong',
-                    error: err
-                });
+    try {
+        for (var i in data) {
+            var newDate = new Date((data[i].Date - (25567 + 2)) * 86400 * 1000);
+            var convertedDate = dateFormat(newDate, "mm/dd/yyyy");
+            var userObj = {
+                date: convertedDate,
+                shift1: data[i].Shift1,
+                shift2: data[i].Shift2,
+                shift3: data[i].Shift3,
+                shift4: data[i].Shift4,
+                remark: data[i].Remark
             }
-        })
+            admin.create(userObj, (err, user) => {
+                if (err) {
+                    res.json({
+                        success: false,
+                        message: 'Something went Wrong',
+                        error: err
+                    });
+                }
+            })
+        }
+        for (var i in data) {
+            var newDate = new Date((data[i].Date - (25567 + 2)) * 86400 * 1000);
+            var convertedDate = dateFormat(newDate, "mm/dd/yyyy");
+            //console.log(conDate); 
+            arr[i] =
+                {
+                    date: convertedDate,
+                    shift_name1: '4:00PM - 4:00AM',
+                    car_req1: data[i].Shift1,
+                    shift_name2: '6:00PM - 6:00AM',
+                    car_req2: data[i].Shift2,
+                    shift_name3: '9:00PM - 9:00AM',
+                    car_req3: data[i].Shift3,
+                    shift_name4: '10:00PM - 10:00AM',
+                    car_req4: data[i].Shift4,
+                    remarks: data[i].Remark
+                }
+        }
+        res.json({
+            success: true,
+            message: "Shift Details Uploaded",
+            result: arr
+        });
     }
-      for (var i in data) {
-          var newDate =  new Date((data[i].Date - (25567 + 2))*86400*1000);
-          var convertedDate = dateFormat(newDate, "mm/dd/yyyy"); 
-          //console.log(conDate); 
-        arr[i] =
-            {                 
-            date: convertedDate,  
-            shift_name1:'4:00PM - 4:00AM',        
-            car_req1:data[i].Shift1,
-            shift_name2:'6:00PM - 6:00AM',  
-            car_req2:data[i].Shift2,
-            shift_name3:'9:00PM - 9:00AM',  
-            car_req3:data[i].Shift3,
-            shift_name4:'10:00PM - 10:00AM',  
-            car_req4:data[i].Shift4,
-            remarks:data[i].Remark
-            }               
+    catch{
+        res.json({
+            success: false,
+            message: 'Invalid Date Format',
+            //error: err
+        });
     }
-    res.json({
-        success: true,
-        message: "Shift Details Uploaded",
-        result: arr
-    });
 });
 
 module.exports = router;
